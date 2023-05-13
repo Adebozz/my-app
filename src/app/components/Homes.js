b31f0f3121730f1e2aa72231132d4eddfd7ed366
Adebozz <ademolaadeigbe3@gmail.com>
2023-05-13 22:02:51 +0100

    first commit

--- src/app/components/Homes.js
+++ src/app/components/Homes.js
@@ -7,8 +7,8 @@ const Homes = () => {
       className="flex flex-col w-full min-h-screen bg-gradient-to-b from-black to-gray-500"
     >
       <div className="w-full h-full text-white">
-        <div className="w-full align-center flex-col flex justify-center">
-          <h1 className="text-white text-4xl">Our Goal</h1>
+        <div className="w-full align-center flex-col flex justify-center p-80">
+          <h1 className="text-white text-6xl">Our Goal</h1>
           <p>Quality education at your finger tips</p>
         </div>
       </div>
